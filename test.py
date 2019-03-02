9fc3ac323d4d2cbdf2fca20e842c61a5b4ca7a75
Matthew <Casual@eitx-matthew.home>
2019-03-02 21:51:46 +0000

    Re-added blueprint factory and fun test cases.

--- test.py
+++ test.py
@@ -2,17 +2,22 @@ from PIL import Image
 import json
 import math
 
-from ddl import ArtpackFactory, AssetFactory, Renderer
+from ddl import ArtpackFactory, BlueprintFactory, Renderer
 
 artpack = ArtpackFactory.load('example_isometric')
-test = Renderer(sub_assets=artpack.blueprints['floor_2x2_exact'].get_sub_assets(500,0,artpack,294,170))
+test = Renderer(sub_assets=artpack.blueprints['floor_2x2_exact'].get_sub_assets(500,0,artpack))
 test.render()
 
-assetfac=AssetFactory(artpack,artpack.data["grid"])
-assetfac.new_asset('floor_1x2_exact','floor')
-assetfac.add_asset("floor_1x1_exact",0,0)
-assetfac.add_asset("floor_1x1_exact",0,1)
-floor4=assetfac.spy_asset()
+assetfac=BlueprintFactory(artpack,"isometric")
+assetfac.new_blueprint('floor_1x2_exact','floor')
+assetfac.add_image("floor_1x1_exact",0,0)
+assetfac.add_image("floor_1x1_exact",0,1)
+floor4=assetfac.get_blueprint()
+assetfac.add_blueprint("floor_2x2_exact",1,0)
+floor5=assetfac.pull_blueprint()
 
 renderer2 = Renderer(sub_assets=floor4.get_sub_assets(500,0,artpack))
 renderer2.render()
+
+renderer3 = Renderer(sub_assets=floor5.get_sub_assets(500,0,artpack))
+renderer3.render()
