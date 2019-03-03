92265366031fa876426ae6ddb0f00e6d1e1cdd68
Matthew Jackson <17379413+mmjackson90@users.noreply.github.com>
2019-03-03 09:01:32 +0000

    Seperated concerns of finding images within a blueprint and positioning images with respect to a given co-ordinate system. Also tidied a couple of things.

--- ddl/__init__.py
+++ ddl/__init__.py
@@ -22,76 +22,52 @@ class Artpack:
         self.artpack = {
             'name': name,
             'grid': artpack['grid'],
-            'blueprints': artpack['blueprints']
         }
 
         for image in self.imagepack['images']:
             self.images[image['id']] = Image_asset(image, artpack_name=name)
 
-        for blueprint in self.artpack['blueprints']:
-            self.blueprints[blueprint['id']] = Blueprint(blueprint)
+        for blueprint in artpack['blueprints']:
+            self.blueprints[blueprint['id']] = Blueprint(blueprint, artpack_name=name)
 
 
 class Image_asset:
     def __init__(self, data, artpack_name):
+        self.artpack_name=artpack_name
         self.data = data
         self.image = Image.open('artpacks/' + artpack_name + '/art/' + self.data["image"])
-
-
-class Blueprint:
-    def __init__(self, data):
-        self.data = data
-        if "sub_assets" not in self.data.keys():
-            raise Exception('Blueprint {} has no sub assets.',self.data["name"])
 
     def show(self):
         self.image.show()
 
-    def get_sub_asset_location_isometric(self,x,y,grid_square_pixel_width,grid_square_pixel_height):
-            pixel_x=(y*math.ceil(grid_square_pixel_width/2))-(x*math.floor(grid_square_pixel_width/2))
-            pixel_y=(x*math.ceil(grid_square_pixel_height/2))+(y*math.floor(grid_square_pixel_height/2))
-            return (pixel_x, pixel_y)
 
-    def get_sub_asset_location_classic(self,x,y,grid_square_pixel_width,grid_square_pixel_height):
-            pixel_x=x*grid_square_pixel_width
-            pixel_y=y*grid_square_pixel_height
-            return (pixel_x, pixel_y)
+class Blueprint:
+    def __init__(self, data, artpack_name):
+        self.data = data
+        self.artpack_name=artpack_name #The artpack this blueprint is a part of
+        if "sub_assets" not in self.data.keys():
+            raise Exception('Blueprint {} has no sub assets.',self.data["name"])
 
-    def get_sub_assets(self, offset_x, offset_y, artpack):
-        grid_square_pixel_width=artpack.artpack['grid']['width']
-        grid_square_pixel_height=artpack.artpack['grid']['height']
+    def get_image_location_list(self, offset_x, offset_y, artpack):
         image_location_list=[]
         for sub_asset in self.data["sub_assets"]:
-            if self.data['grid_type']=="isometric":
-                pixel_x, pixel_y = self.get_sub_asset_location_isometric(sub_asset["x"],sub_asset["y"], grid_square_pixel_width, grid_square_pixel_height)
-            else:
-                pixel_x, pixel_y = self.get_sub_asset_location_classic(sub_asset["x"],sub_asset["y"], grid_square_pixel_width, grid_square_pixel_height)
             if sub_asset["type"]=="image":
-                image_location_list=image_location_list+[(artpack.images[sub_asset["image_id"]],pixel_x+offset_x,pixel_y+offset_y)]
+                image_location_list=image_location_list+[(artpack.images[sub_asset["image_id"]],sub_asset["x"]+offset_x,sub_asset["y"]+offset_y)]
             else:
-                image_location_list=image_location_list+artpack.blueprints[sub_asset["blueprint_id"]].get_sub_assets(pixel_x+offset_x,pixel_y+offset_y,artpack)
+                image_location_list=image_location_list+artpack.blueprints[sub_asset["blueprint_id"]].get_image_location_list(sub_asset["x"]+offset_x,sub_asset["y"]+offset_y,artpack)
         return image_location_list
 
 class BlueprintFactory:
     def __init__(self, artpack, grid_type):
         self.artpack=artpack
         self.grid_type=grid_type
-        self.current_asset=False
-        self.id = None
-        self.layer = None
-        self.top_left = None
-        self.name = None
-        self.horizontally_flippable = None
-        self.vertically_flippable = None
-        self.tags = None
-        self.connections = None
-        self.sub_assets = None
+        self.clear_blueprint()
 
     def new_blueprint(self, id, layer, top_left = (0, 0), name='',
     horizontally_flippable=True, vertically_flippable=True,
     tags=[],connections=[], sub_assets=[]):
         if self.current_asset:
-            raise Exception('This factory is currently building another asset. Please finalise that asset before starting a new one.')
+            raise Exception('This factory is currently building another blueprint. Please finalise that asset before starting a new one.')
         self.current_asset=True
         self.id = id
         self.layer = layer
@@ -128,7 +104,7 @@ class BlueprintFactory:
             "vertically_flippable": self.vertically_flippable,
             "tags": self.tags
         }
-        return Blueprint(data)
+        return Blueprint(data,self.artpack.artpack['name'])
 
     def clear_blueprint(self):
         self.current_asset=False
@@ -147,24 +123,50 @@ class BlueprintFactory:
         self.clear_blueprint()
         return image_list
 
+class Positioner:
+    def __init__(self, grid_definition):
+        self.grid_definition=grid_definition
+        #MONKEYPAAAAAAAAAAAAAAATCH!!! (I think it's neater here, given that we know the co-ordinate system.
+        if grid_definition['type']=="isometric":
+            self.get_location_in_pixels = self.get_locations_isometric
+        else:
+            self.get_location_in_pixels = self.get_locations_classic
+
+    def get_locations_isometric(self,x,y,grid_square_pixel_width,grid_square_pixel_height):
+            pixel_x=(y*math.ceil(grid_square_pixel_width/2))-(x*math.floor(grid_square_pixel_width/2))
+            pixel_y=(x*math.ceil(grid_square_pixel_height/2))+(y*math.floor(grid_square_pixel_height/2))
+            return (pixel_x, pixel_y)
+
+    def get_locations_classic(self,x,y,grid_square_pixel_width,grid_square_pixel_height):
+            pixel_x=x*grid_square_pixel_width
+            pixel_y=y*grid_square_pixel_height
+            return (pixel_x, pixel_y)
+
+    def get_image_pixel_list(self, pixel_offset_x, pixel_offset_y, image_location_list):
+        #Worry about performance later.
+        image_pixel_list=[]
+        for image, x, y in image_location_list:
+            pixel_x, pixel_y=self.get_location_in_pixels(x,y,self.grid_definition['width'],self.grid_definition['height'])
+            pixel_x=pixel_x-image.data['top_left']['x']+pixel_offset_x
+            pixel_y=pixel_y-image.data['top_left']['y']+pixel_offset_y
+            image_pixel_list=image_pixel_list+[(image,pixel_x,pixel_y)]
+        return image_pixel_list
+
 
 class Renderer:
-    def __init__(self, width=1000, height=1000, sub_assets=[]):
+    def __init__(self, grid_type= 'isometric', width=1000, height=1000, image_pixel_list=[]):
         self.image_pixel_width = width
         self.image_pixel_height = height
-        self.sub_assets=sub_assets
+        self.grid_type = grid_type
+        self.image_pixel_list=image_pixel_list
         self.centre_line=round(width/2)
         self.initialise_image(self.image_pixel_width, self.image_pixel_height)
 
     def initialise_image(self, width, height):
         self.image = Image.new('RGBA', (width, height),(255,0,0,0))
 
-    def add_sub_asset_list(self, sub_asset_list):
-        self.sub_assets=sub_asset_list+self.sub_assets
-
-    #This will need moving out to the asset factory, but for now leaving it here.
-    def add_asset(self, asset, x, y, artpack):
-        self.sub_assets=asset.get_sub_assets(x,y,artpack)+self.sub_assets
+    def add_image_pixel_list(self, image_pixel_list):
+        self.image_pixel_list=image_pixel_list+self.image_pixel_list
 
     def add_to_image(self, asset, x, y):
         final_x=x-asset.data["top_left"]["x"]
@@ -172,6 +174,6 @@ class Renderer:
         self.image.paste(asset.image,(final_x,final_y),asset.image)
 
     def render(self):
-        for asset, x, y in self.sub_assets:
+        for asset, x, y in self.image_pixel_list:
             self.add_to_image(asset, x, y)
         self.image.show()
--- test.py
+++ test.py
@@ -2,17 +2,20 @@ from PIL import Image
 import json
 import math
 
-from ddl import ArtpackFactory, BlueprintFactory, Renderer
+from ddl import ArtpackFactory, BlueprintFactory, Renderer, Positioner
 
 artpack = ArtpackFactory.load('example_isometric')
-test = Renderer(sub_assets=artpack.blueprints['floor_2x2_exact'].get_sub_assets(500,0,artpack))
-test.render()
+positioner = Positioner(artpack.artpack['grid'])
+image_location_list=artpack.blueprints['floor_2x2_exact'].get_image_location_list(0,0,artpack)
+renderer = Renderer(image_pixel_list=positioner.get_image_pixel_list(500,0,image_location_list))
+renderer.render()
 
 assetfac=BlueprintFactory(artpack,"isometric")
 assetfac.new_blueprint('floor_1x2_exact','floor')
 assetfac.add_image("floor_1x1_exact",0,0)
 assetfac.add_image("floor_1x1_exact",0,1)
 floor4=assetfac.pull_blueprint()
+
 assetfac.new_blueprint('twiddle_1','floor')
 assetfac.add_blueprint("floor_2x2_exact",0,0)
 assetfac.add_image("floor_1x1_exact",2,1)
@@ -21,8 +24,10 @@ assetfac.add_image("floor_1x1_exact",2,3)
 assetfac.add_blueprint("floor_2x2_exact",3,3)
 floor5=assetfac.pull_blueprint()
 
-renderer2 = Renderer(sub_assets=floor4.get_sub_assets(500,0,artpack))
+print(floor5.get_image_location_list(1,1,artpack))
+
+renderer2 = Renderer(image_pixel_list=positioner.get_image_pixel_list(500,0,floor4.get_image_location_list(0,0,artpack)))
 renderer2.render()
 
-renderer3 = Renderer(sub_assets=floor5.get_sub_assets(500,0,artpack))
+renderer3 = Renderer(image_pixel_list=positioner.get_image_pixel_list(500,0,floor5.get_image_location_list(0,0,artpack)))
 renderer3.render()
