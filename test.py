d041fdd912adf585c842183963e5f9adc95b1d4a
Matthew <Casual@eitx-matthew.home>
2019-03-03 17:01:09 +0000

    Adds scaling to images and gives artpack the ability to control scaling if passed another grid definition.

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ artpack = ArtpackFactory.load('example_isometric')
 positioner = Positioner(artpack.artpack['grid'])
 image_location_list=artpack.blueprints['floor_2x2_exact'].get_image_location_list(0,0,artpack)
 renderer = Renderer(image_pixel_list=positioner.get_image_pixel_list(500,0,image_location_list))
-#renderer.render()
+renderer.render()
 
 assetfac=BlueprintFactory(artpack,"isometric")
 assetfac.new_blueprint('floor_1x2_exact','floor')
@@ -27,10 +27,10 @@ floor5=assetfac.pull_blueprint()
 print(floor5.get_image_location_list(1,1,artpack))
 
 renderer2 = Renderer(image_pixel_list=positioner.get_image_pixel_list(500,0,floor4.get_image_location_list(0,0,artpack)))
-#renderer2.render()
+renderer2.render()
 
 renderer3 = Renderer(image_pixel_list=positioner.get_image_pixel_list(500,0,floor5.get_image_location_list(0,0,artpack)))
-#renderer3.render()
+renderer3.render()
 
 assetfac.new_blueprint('fuzzy','floor')
 assetfac.add_image("floor_1x1_exact",0,0)
@@ -49,3 +49,12 @@ wall=assetfac.pull_blueprint()
 
 renderer5 = Renderer(image_pixel_list=positioner.get_image_pixel_list(500,500,wall.get_image_location_list(0,0,artpack)))
 renderer5.render()
+
+#Will alter all of low_res_artpack's images to be the same size as artpack's
+low_res_artpack = ArtpackFactory.load('low_res_isometric')
+low_res_artpack.resize_images(artpack.artpack['grid'])
+image_location_list2=low_res_artpack.blueprints['floor_2x2_low_res'].get_image_location_list(1,1,low_res_artpack)
+#toto: Why is this backwards?
+renderer6=Renderer(image_pixel_list=positioner.get_image_pixel_list(500,0,image_location_list2))
+renderer6.add_image_pixel_list(image_pixel_list=positioner.get_image_pixel_list(500,0,image_location_list))
+renderer6.render()
