4a5c2919553aca5b6721c155cce7dd66929eaa66
Matthew Jackson <17379413+mmjackson90@users.noreply.github.com>
2019-03-04 08:53:57 +0000

    Aaaand fix the issues that Atom didn't flag up as syntax errors for some reason.

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from ddl import ArtpackFactory, BlueprintFactory, Renderer, Positioner
 
 artpack = ArtpackFactory.load('example_isometric')
 positioner = Positioner(artpack.artpack['grid'])
-image_location_list = artpack.blueprints['floor_2x2_exact']
-.get_image_location_list(0, 0, artpack)
+floor1 = artpack.blueprints['floor_2x2_exact']
+image_location_list = floor1.get_image_location_list(0, 0, artpack)
 renderer = Renderer(image_pixel_list=positioner
                     .get_image_pixel_list(500, 0, image_location_list))
 renderer.render()
@@ -23,19 +23,17 @@ assetfac.add_blueprint("floor_2x2_exact", 3, 3)
 floor5 = assetfac.pull_blueprint()
 
 print(floor5.get_image_location_list(1, 1, artpack))
-
-renderer2 = Renderer(image_pixel_list=positioner
-                     .get_image_pixel_list(500, 0,
-                                           floor4
-                                           .get_image_location_list(0, 0,
-                                                                    artpack)))
+image_location_list2 = floor4.get_image_location_list(0, 0, artpack)
+image_pixel_list2 = positioner.get_image_pixel_list(500, 0,
+                                                    image_location_list2)
+renderer2 = Renderer(image_pixel_list=image_pixel_list2)
 renderer2.render()
 
-renderer3 = Renderer(image_pixel_list=positioner
-                     .get_image_pixel_list(500, 0,
-                                           floor5
-                                           .get_image_location_list(0, 0,
-                                                                    artpack)))
+
+image_location_list3 = floor5.get_image_location_list(0, 0, artpack)
+image_pixel_list3 = positioner.get_image_pixel_list(500, 0,
+                                                    image_location_list3)
+renderer3 = Renderer(image_pixel_list=image_pixel_list3)
 renderer3.render()
 
 assetfac.new_blueprint('fuzzy', 'floor')
@@ -61,12 +59,13 @@ renderer5.render()
 # Will alter all of low_res_artpack's images to be the same size as artpack's
 low_res_artpack = ArtpackFactory.load('low_res_isometric')
 low_res_artpack.resize_images(artpack.artpack['grid'])
-image_location_list2 = low_res_artpack.blueprints['floor_2x2_low_res']
-.get_image_location_list(
-    1, 1, low_res_artpack)
+low_res_floor = low_res_artpack.blueprints['floor_2x2_low_res']
+image_location_list6 = low_res_floor.get_image_location_list(1, 1,
+                                                             low_res_artpack)
+image_pixel_list_6 = positioner.get_image_pixel_list(500, 0,
+                                                     image_location_list6)
 # toto: Why is this backwards?
-renderer6 = Renderer(image_pixel_list=positioner
-                     .get_image_pixel_list(500, 0, image_location_list2))
+renderer6 = Renderer(image_pixel_list=image_pixel_list_6)
 renderer6.add_image_pixel_list(
     image_pixel_list=positioner
     .get_image_pixel_list(500, 0, image_location_list))
