427c9ec6bdba3d3fb0fb11e85e7829d5e4b95ad8
Matthew Jackson <17379413+mmjackson90@users.noreply.github.com>
2019-03-08 07:08:00 +0000

    Fix CQ issues in test files.

--- tests/test_outputs.py
+++ tests/test_outputs.py
@@ -28,10 +28,10 @@ def test_old_tests():
     floor4 = component_factory.pull_component()
 
     image_location_list2 = floor4.get_image_location_list(0, 0, assetpack)
-    image_pixel_list2 = positioner.get_image_pixel_list(0, 0, image_location_list2)
+    image_pixel_list2 = positioner.\
+        get_image_pixel_list(0, 0, image_location_list2)
     renderer2 = Renderer(image_pixel_list=image_pixel_list2)
     renderer2.output('screen')
-
 
     component_factory.new_component('twiddle_1', 'floor')
     component_factory.add_component("floor-2x2-exact", 0, 0)
@@ -65,14 +65,15 @@ def test_old_tests():
         0, 0, wall.get_image_location_list(0, 0, assetpack)))
     renderer5.output('screen')
 
-    # Will alter all of low_res_assetpacks images to be the same size as assetpacks
+    # Will alter all of low_res_assetpacks images
+    # to be the same size as assetpacks
     low_res_assetpack = AssetpackFactory.load('low_res_isometric')
     low_res_assetpack.resize_images(assetpack.projection)
     low_res_floor = low_res_assetpack.components['floor-2x2-low-res']
-    image_location_list6 = low_res_floor.get_image_location_list(1, 1,
-                                                                 low_res_assetpack)
-    image_pixel_list6 = positioner.get_image_pixel_list(0, 0,
-                                                        image_location_list6)
+    image_location_list6 = low_res_floor.\
+        get_image_location_list(1, 1, low_res_assetpack)
+    image_pixel_list6 = positioner.\
+        get_image_pixel_list(0, 0, image_location_list6)
     # toto: Why is this backwards?
     renderer6 = Renderer(image_pixel_list=image_pixel_list6)
     renderer6.add_image_pixel_list(
@@ -81,8 +82,8 @@ def test_old_tests():
     renderer6.output('screen')
 
     # Adds in tiny boxes using a native grid (1 square =1/10th the big grid)
-    # Then rescales the assetpack and adds in the same component on the big grid
-    # much more accurately.
+    # Then rescales the assetpack and adds in the same component on the big
+    # grid much more accurately.
     prop_assetpack = AssetpackFactory.load('example_props')
     boxes = prop_assetpack.components['many-boxes']
     image_location_list7 = boxes.get_image_location_list(0, 0, prop_assetpack)
@@ -91,10 +92,11 @@ def test_old_tests():
                                                          image_location_list7)
 
     prop_assetpack.rescale_pack(assetpack.projection)
-    image_location_list7_1 = boxes.get_image_location_list(0, 1, prop_assetpack)
+    image_location_list7_1 = boxes.\
+        get_image_location_list(0, 1, prop_assetpack)
 
-    image_pixel_list7_1 = positioner.get_image_pixel_list(0, 0,
-                                                          image_location_list7_1)
+    image_pixel_list7_1 = positioner.\
+        get_image_pixel_list(0, 0, image_location_list7_1)
     # todo: Why is this backwards?
     renderer7 = Renderer(image_pixel_list=image_pixel_list7_1)
     renderer7.add_image_pixel_list(image_pixel_list=image_pixel_list7)
