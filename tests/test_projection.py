7f24defc90afdb5dc8f3684febdab99e9c09b7d5
Matthew Jackson <17379413+mmjackson90@users.noreply.github.com>
2019-03-08 21:40:48 +0000

    Projection now has a full suite of unit tests. Enjoy.

--- tests/test_projection.py
+++ tests/test_projection.py
@@ -39,17 +39,58 @@ def test_resize_images():
     assert image.top_left['y'] == 30
 
 
-def rescale_components(self, components, desired_projection):
+def test_rescale_components():
     """Tests that a list of components rescales correctly"""
+    data = {
+        "name": "2x2 Floor exact",
+        "id": "floor-2x2-exact",
+        "parts": [
+            {
+                "type": "image",
+                "image_id": "floor-1x1-exact",
+                "x": 0,
+                "y": 0
+            },
+            {
+                "type": "image",
+                "image_id": "floor-1x1-exact",
+                "x": 1,
+                "y": 2
+            }
+        ],
+        "tags": [
+            "example"
+        ]
+    }
+
+    component = Component(data, "example_isometric")
+    components = {"test": component}
+    projection1 = Projection(1, 1)
+    projection2 = Projection(5, 5)
+    projection1.rescale_components(components, projection2)
+    component.data["parts"][0]["x"] == 0
+    component.data["parts"][0]["y"] == 0
+    component.data["parts"][1]["x"] == 1/5
+    component.data["parts"][1]["y"] == 2/5
 
 
-def get_image_pixel_list(self,
-                         grid_offset_x,
-                         grid_offset_y,
-                         image_location_list):
+def test_get_image_pixel_list():
     """Goes through a list of images and grid co-ordinates and returns a
      list of images and pixel co-ordinates."""
-
+    image_data = {"name": "test",
+                  "id": "test",
+                  "top_left": {"x": 152, "y": 6},
+                  "image": "1x1_floor_fuzzy.png"}
+    image = ImageAsset(image_data, "example_isometric")
+    projection1 = TopDownProjection(10, 10)
+    image_location_list = [(image, 0, 0), (image, 1, 2)]
+    pixel_list = projection1.get_image_pixel_list(1, 3, image_location_list)
+    assert isinstance(pixel_list[0][0], ImageAsset)
+    assert pixel_list[0][1] == 10
+    assert pixel_list[0][2] == 30
+    assert isinstance(pixel_list[1][0], ImageAsset)
+    assert pixel_list[1][1] == 20
+    assert pixel_list[1][2] == 50
 
 
 def test_isometric_get_pixels():
