4bc9be618231021d5d0439d44fdb1d340a80e11c
Matthew Jackson <17379413+mmjackson90@users.noreply.github.com>
2019-03-09 16:58:54 +0000

    Component tests. Currently missing the get_location_list test prior to refactor.

--- tests/test_component.py
+++ tests/test_component.py
@@ -34,3 +34,40 @@ def test_component_init():
         raise AssertionError()
     if not len(component.parts) == 2:
         raise AssertionError()
+
+
+def test_rescale_component():
+    data = {
+        "name": "2x2 Floor exact",
+        "id": "floor-2x2-exact",
+        "parts": [
+            {
+                "type": "image",
+                "image_id": "floor-1x1-exact",
+                "x": 0,
+                "y": 0
+            },
+            {
+                "type": "image",
+                "image_id": "floor-1x1-exact",
+                "x": 2,
+                "y": 3
+            }
+        ],
+        "tags": [
+            "example"
+        ]
+    }
+    component = Component(data, 'test_assetpack_name')
+    component.rescale(2, 3)
+    if not component.data["parts"][0]["x"] == 0:
+        raise AssertionError()
+    if not component.data["parts"][0]["y"] == 0:
+        raise AssertionError()
+    if not component.data["parts"][1]["x"] == 1:
+        raise AssertionError()
+    if not component.data["parts"][1]["y"] == 1:
+        raise AssertionError()
+
+
+def test_get_location_list():
