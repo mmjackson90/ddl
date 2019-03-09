2e148819b5d484caeb28511933c762bf2e8cfea6
Matthew Jackson <17379413+mmjackson90@users.noreply.github.com>
2019-03-09 12:00:49 +0000

    Testing the assetpack methods.

--- tests/test_assetpack.py
+++ tests/test_assetpack.py
@@ -3,6 +3,7 @@ Tests Assetpacks
 """
 
 from ddl import AssetpackFactory, Assetpack
+from ddl.projection import Projection
 
 
 def test_factory_creates_assetpack():
@@ -10,3 +11,27 @@ def test_factory_creates_assetpack():
     assetpack = AssetpackFactory.load('example_isometric')
     if not isinstance(assetpack, Assetpack):
         raise AssertionError()
+
+
+def test_assetpack_resize():
+    """Test resizing an assetpack's images to match a new projection."""
+    assetpack = AssetpackFactory.load('example_isometric')
+    projection2 = Projection(29, 17)
+    assetpack.resize_images(projection2)
+    assert assetpack.projection.width == 29
+    assert assetpack.projection.height == 17
+    assert assetpack.images['floor-1x1-exact'].image.width == 29
+    assert assetpack.images['floor-1x1-exact'].image.height == 19
+
+
+def test_assetpack_resize():
+    """Test rescaling an assetpack's components to match a new projection."""
+    assetpack = AssetpackFactory.load('example_isometric')
+    projection2 = Projection(29, 17)
+    assetpack.rescale_components(projection2)
+    assert assetpack.projection.width == 29
+    assert assetpack.projection.height == 17
+    assert assetpack.components['floor-2x2-exact'].parts[0]['x'] == 0
+    assert assetpack.components['floor-2x2-exact'].parts[0]['y'] == 0
+    assert assetpack.components['floor-2x2-exact'].parts[3]['x'] == 294/29
+    assert assetpack.components['floor-2x2-exact'].parts[3]['y'] == 10
