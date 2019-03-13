56750c1c09767ad15058791e178226cf090d9ac4
Matthew Jackson <17379413+mmjackson90@users.noreply.github.com>
2019-03-13 15:15:14 +0000

    Test that validator craps out with invalid files.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -3,6 +3,8 @@ Tests example files to ensure we're shipping valid ones.
 """
 
 from ddl.validator import Validator
+from pytest import raises
+from jsonschema import ValidationError
 
 
 def test_eg_iso_pack():
@@ -57,3 +59,24 @@ def test_eg_props_components():
     """ assetpacks/example_props/components.json """
     Validator.validate_file('assetpacks/example_props/components.json',
                             'components')
+
+
+def test_eg_failures():
+    """ assetpacks/example_json_fail/pack.json """
+    with raises(ValidationError):
+        Validator.validate_file('assetpacks/example_json_fail/pack.json',
+                                'pack')
+
+
+def test_eg_failures_images():
+    """ assetpacks/example_json_fail/images.json """
+    with raises(ValidationError):
+        Validator.validate_file('assetpacks/example_json_fail/images.json',
+                                'images')
+
+
+def test_eg_failures_components():
+    """ assetpacks/example_json_fail/components.json """
+    with raises(Exception):
+        Validator.validate_file('assetpacks/example_json_fail/components.json',
+                                'components')
